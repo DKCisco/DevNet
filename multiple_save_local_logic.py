8fdb9e65e48e1d0547d8473a6eb9325fb5fb4902
DKCisco <dillan.k.craig@gmail.com>
2023-12-27 20:50:20 -0700

    Multiple IP address local login local save config script

---
+++ multiple_save_local_logic.py
@@ -0,0 +1,67 @@
+import time
+import paramiko
+import maskpass
+
+def read_from_channel(channel):
+    output = ""
+    while True:
+        if channel.recv_ready():
+            output += channel.recv(65535).decode()
+        else:
+            time.sleep(2)
+            if not channel.recv_ready():
+                break
+    return output
+
+is_local_login = input('Is this a local login? (yes/no): ').strip().lower()
+
+if is_local_login == 'yes':
+    admin_creds = input('Enter the username for local login: ')
+    admin_pw = maskpass.askpass(prompt="Enter PW for local login: ", mask="#*")
+    admin_en = maskpass.askpass(prompt="Enter enable PW: ", mask="*#")
+else:
+    admin_creds = input('Enter the username for non-local login: ')
+    admin_pw = maskpass.askpass(prompt="Enter PW for non-local login: ", mask="#*")
+    admin_en = None
+
+file_save_name = input('What do you want to save the file name as: ')
+
+# Read the IP addresses from the .txt file
+with open('ip_addresses.txt', 'r') as file:
+    ip_addresses = file.read().splitlines()
+
+for device_ip in ip_addresses:
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(device_ip, username=admin_creds, password=admin_pw, look_for_keys=False, allow_agent=False)
+
+        # Using Paramiko's invoke_shell method to interact with the device
+        channel = ssh.invoke_shell()
+
+        if is_local_login == 'yes':
+            # Sending enable command for local login
+            channel.send('enable\n')
+            time.sleep(1)  # Waiting for the enable password prompt
+            channel.send(admin_en + '\n')
+            time.sleep(1)  # Waiting for the command to be processed
+
+        # Setting terminal length to avoid pagination and fetching running config
+        channel.send('terminal length 0\n')
+        time.sleep(1)
+        channel.send('show access-session\n')
+        time.sleep(2)  # Giving some time for the command to execute
+
+        # Capturing the output
+        output = read_from_channel(channel)
+
+        # Closing the SSH connection
+        ssh.close()
+
+        # Saving the output to a file
+        with open(file_save_name + "_" + device_ip + ".txt", "w") as file:
+            file.write(output)
+
+        print(output)
+    except Exception as e:
+        print(f"Failed to connect to {device_ip}. Error: {str(e)}")
